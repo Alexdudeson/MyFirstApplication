29a8ce29257becd96a6cf0122329543760f919d8
Alexey <alexxd91@gmail.com>
2021-02-12 20:09:48 +0300

    Initial commit, Counter app (does not work prop)

---
+++ app/src/main/java/myfirstapplication/sukkkksess/MainActivity.kt
@@ -0,0 +1,35 @@
+package myfirstapplication.sukkkksess
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+/*fun countMe (view: View)
+{
+    val countString= TextView.text.toString ()
+    var count: Int=Integer.parseInt(countString)
+    count++
+
+    TextView.text=count.toString()
+
+
+}
+
+    }
+
+fun button2_counter_plus_2Me (view: View, button2_counter_plus_2: Any)
+{
+    val mybutton2_counter_plus_2= button2_counter_plus_2.makeText (context: this,
+    text:"Doesn't work for now", button2_counter_plus_2.LENGTH_SHORT)
+    mybutton2_counter_plus_2.show()
+}
+*/
+}
